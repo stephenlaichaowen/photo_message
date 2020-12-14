88ddfc9c27dc4a5e5cb4e14e17111caf4135e5f2
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-14 18:29:21 +0800

    'commit'

--- components/Messages/MessageList.vue
+++ components/Messages/MessageList.vue
@@ -6,12 +6,12 @@
 
 <style scoped>
 #message-list {
-  height: calc(100vh - 6.25rem);
+  /* height: calc(100vh - 6.25rem);
   position: fixed;
   top: 3.125rem;
   bottom: 3.125rem;
   left: 0;  
   z-index: 100;
-  overflow-y: scroll;    
+  overflow-y: scroll;     */
 }
 </style>
