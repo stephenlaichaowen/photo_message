1c85cb3e31d329e11831bb4a4b38b01b1564e8db
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-15 07:05:51 +0800

    'commit'

--- components/Header/SearchMenu.vue
+++ components/Header/SearchMenu.vue
@@ -11,7 +11,7 @@
         v-model="keyword"
         id="search-input"
         placeholder="Search..."
-        class="flex-grow-1 w-100 align-middle pr-5 pl-1"
+        class="flex-grow-1 w-100 rounded align-middle pr-5 pl-1"
       />
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Arrow_icon.svg/1200px-Arrow_icon.svg.png"
