fd0fe5653d1a1dd84c2e56109d25fb878213cd4b
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-15 05:47:11 +0800

    'commit'

--- components/Header/SearchMenu.vue
+++ components/Header/SearchMenu.vue
@@ -6,7 +6,8 @@
       v-if="searchMenuState"
       class="bg-dark"
     >
-      <input type="text" v-model="keyword" id="search-input" class="flex-grow-1 w-100 align-middle pr-5" />
+      <!-- <input type="text" v-model="keyword" id="search-input" class="flex-grow-1 w-100 align-middle pr-5" /> -->
+      <input type="text" v-model="keyword" id="search-input" placeholder="Search..." />
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Arrow_icon.svg/1200px-Arrow_icon.svg.png" id="icon-search" @click="setSearchKeyword" />
     </div>
   </transition>
