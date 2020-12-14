2547d4ea352ee2fe0338c660f033cffe05157dc7
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-15 07:49:49 +0800

    'commit'

--- components/Header/SearchMenu.vue
+++ components/Header/SearchMenu.vue
@@ -10,7 +10,7 @@
         v-model="keyword"
         id="search-input"
         placeholder="Search..."
-        class="flex-grow-1 w-100 align-middle"
+        class="flex-grow-1 w-50 align-middle"
       />
       <img
         src="https://www.freeiconspng.com/uploads/black-close-icon-3.png"
