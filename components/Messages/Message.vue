84bf2c37cb6722e79af227444e3459fc116b26a9
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-15 09:49:01 +0800

    'commit'

--- components/Messages/Message.vue
+++ components/Messages/Message.vue
@@ -33,7 +33,7 @@
       <div
         id="message"
         ref="message"
-        class="row rounded mx-2 my-3"
+        class="row rounded ml-2 mr-5 my-2"
         v-for="(item, idx) in filteredMessages"
         :key="item.caption"
         v-hammer:swipe.right="() => removeMessage(item.id)"
