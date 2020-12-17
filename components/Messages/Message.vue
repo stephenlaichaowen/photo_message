2075d942dff65d8fb437286237f4d6e23271f024
stephenlaichaowen <stephenlaitest@gmail.com>
2020-12-17 09:01:29 +0800

    'commit'

--- components/Messages/Message.vue
+++ components/Messages/Message.vue
@@ -161,7 +161,7 @@ export default {
   background: rgba(255, 255, 255, 0.05);
   /* overflow: hidden; */
   box-shadow: 0.0313rem 0.0313rem 0.125rem rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(0.3125rem);
+  /* backdrop-filter: blur(0.3125rem); */
   /* border-radius: 2.5rem; */  
 }
 #message-caption {
